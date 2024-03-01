8a4499359305f381c6e20cd83cd68cdf61a935ab
marovichn <nikolamarovic.11@gmail.com>
2024-03-01 03:02:44 +0100

    demo-2

--- server/server-demo.js
+++ server/server-demo.js
@@ -5,58 +5,52 @@ const Readline = require("@serialport/parser-readline").ReadlineParser;
 const app = express();
 const portName = "COM3"; // Change this to the appropriate port name on your system
 const baudRate = 9600; // Match the baud rate with the one in Arduino code
-const port = new SerialPort({ path: portName, baudRate });
+const port = new SerialPort({path: portName,  baudRate });
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
-function controlStepper(ankle, direction, steps) {
-  port.write(`${ankle}-${direction}-${steps}\n`);
+const dirPin = 2;
+const angPin = 13;
+const keyPin = 8;
+
+
+app.get("/angularcommand/:dirang", (req, res) => {
+  const { dirang } = req.params;
+  const [direction, angle] = dirang.split("-");
+  if (direction === "0" || direction === "1") {
+    controlStepperAngle(direction, angle);
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.status(200).send("Stepper motor moving " + direction);
+  } else {
+    res.status(400).send("Invalid direction");
+  }
+});
+
+function controlStepperAngle(direction, angle) {
+  const steps = angle * 2.22222222222; 
+  // One degree is approximately 2.22 steps
+  port.write(`${angPin},${direction}-${steps}\n`);
 }
-app.get("/angularcommand/:usersettings", (req, res) => {
-  const { usersettings } = req.params;
-  const [ankle, direction, angle] = usersettings.split("-");
-  const steps = angle * 2.22222222222; // Assuming 1 degree step for stepper motor
-  if (direction === "0" || direction === "1") {
-    controlStepper(ankle, direction, steps);
+
+app.get("/move/:direction", (req, res) => {
+  const { direction } = req.params;
+  if (direction === "clockwise" || direction === "counterclockwise") {
+    controlStepper(direction, dirPin);
     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
-    res
-      .status(200)
-      .send(
-        "Stepper motor moving " + direction === "1"
-          ? "clockwise"
-          : "counterclockwise"
-      );
+    res.status(200).send("Stepper motor moving " + direction);
   } else {
     res.status(400).send("Invalid direction");
   }
 });
 
-app.get("/move/:usersettings", (req, res) => {
-  const { usersettings } = req.params;
-  const [ankle, direction, steps] = usersettings.split("-");
-  if (direction === "0" || direction === "1") {
-    controlStepper(ankle, direction, steps);
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res
-      .status(200)
-      .send(
-        "Stepper motor moving " + direction === "1"
-          ? "clockwise"
-          : "counterclockwise"
-      );
-  } else {
-    res.status(400).send("Invalid direction");
-  }
-});
-
-/* app.get("/movekey/:direction", (req, res) => {
+app.get("/movekey/:direction", (req, res) => {
   const { direction } = req.params;
   if (direction === "clockwise" || direction === "counterclockwise") {
     controlStepper(direction, 8);
@@ -69,7 +63,12 @@ app.get("/move/:usersettings", (req, res) => {
   } else {
     res.status(400).send("Invalid direction");
   }
-}); */
+});
+
+function controlStepper(direction, pin) {
+  // Set the spinning direction
+  port.write(`${pin},${direction === "clockwise" ? "1" : "0"}\n`);
+}
 
 parser.on("data", (data) => {
   console.log("Data from Arduino:", data);
